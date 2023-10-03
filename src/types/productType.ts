37aad87feb545cb2ce7b6ad070e4bb9fa1bca9ff
Prasanna_emmadi <prasanna.177@gmail.com>
2023-10-03 13:13:03 +0300

    adding cart,product,user types

---
+++ src/types/productType.ts
@@ -0,0 +1,15 @@
+export interface Category {
+    id: number;
+    name: string;
+    image: string;
+}
+
+
+export interface Product {
+    id: string;
+    title: string;
+    price: number;
+    description: string;
+    category: Category;
+    images: string[];
+}
