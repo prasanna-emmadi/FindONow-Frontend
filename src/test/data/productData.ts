6739edaec20404f86df931b2156b91cabc4c6ec5
Prasanna_emmadi <prasanna.177@gmail.com>
2023-10-08 22:45:49 +0300

    adding react testing library
    adding mock product, user data

---
+++ src/test/data/productData.ts
@@ -0,0 +1,46 @@
+const products = [
+    {
+        id: 29,
+        title: "Elegant Bronze Mouse",
+        price: 794,
+        description:
+            "The Football Is Good For Training And Recreational Purposes",
+        images: [
+            "https://i.imgur.com/imQx3Az.jpeg",
+            "https://i.imgur.com/QEGACen.jpeg",
+            "https://i.imgur.com/M3QKiC5.jpeg",
+        ],
+        creationAt: "2023-10-07T19:16:30.000Z",
+        updatedAt: "2023-10-07T19:16:30.000Z",
+        category: {
+            id: 4,
+            name: "Shoes",
+            image: "https://i.imgur.com/imQx3Az.jpeg",
+            creationAt: "2023-10-07T19:16:30.000Z",
+            updatedAt: "2023-10-07T19:16:30.000Z",
+        },
+    },
+    {
+        id: 31,
+        title: "Incredible Metal Hat",
+        price: 902,
+        description:
+            "New ABC 13 9370, 13.3, 5th Gen CoreA5-8250U, 8GB RAM, 256GB SSD, power UHD Graphics, OS 10 Home, OS Office A & J 2016",
+        images: [
+            "https://i.imgur.com/Y5gHJMd.jpeg",
+            "https://i.imgur.com/5iNAL9T.jpeg",
+            "https://i.imgur.com/RLnJJyQ.jpeg",
+        ],
+        creationAt: "2023-10-07T19:16:30.000Z",
+        updatedAt: "2023-10-07T19:16:30.000Z",
+        category: {
+            id: 5,
+            name: "Others",
+            image: "https://i.imgur.com/0KlqHu9.jpeg",
+            creationAt: "2023-10-07T19:16:30.000Z",
+            updatedAt: "2023-10-07T19:16:30.000Z",
+        },
+    },
+];
+
+export default products;
---
+++ src/test/data/userData.ts
@@ -0,0 +1,24 @@
+const userData = [
+    {
+        id: 1,
+        email: "john@mail.com",
+        password: "changeme",
+        name: "Jhon",
+        role: "customer",
+        avatar: "https://i.imgur.com/lVH533g.jpeg",
+        creationAt: "2023-10-07T19:16:30.000Z",
+        updatedAt: "2023-10-07T19:16:30.000Z",
+    },
+    {
+        id: 2,
+        email: "maria@mail.com",
+        password: "12345",
+        name: "Maria",
+        role: "customer",
+        avatar: "https://i.imgur.com/00qWleT.jpeg",
+        creationAt: "2023-10-07T19:16:30.000Z",
+        updatedAt: "2023-10-07T19:16:30.000Z",
+    },
+];
+
+export default userData;
