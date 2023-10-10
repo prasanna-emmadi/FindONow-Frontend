c6d8452d43da3a7f8081aed7fb14ef0fff20665f
Prasanna_emmadi <prasanna.177@gmail.com>
2023-10-10 19:01:48 +0300

    Removing unused imports

--- src/components/RootPage.tsx
+++ src/components/RootPage.tsx
@@ -9,8 +9,6 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
 import { Outlet } from "react-router-dom";
 import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
