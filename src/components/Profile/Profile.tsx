b8ac9a88c72820a5648b74b51ca612ff68bcf8b8
prasanna-emmadi <prasanna.177@gmail.com>
2023-10-12 03:25:53 +0300

    Minor improvements

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,26 +1,47 @@
-import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import {
+    Card,
+    CardContent,
+    CardMedia,
+    Grid,
+    Paper,
+    Typography,
+} from "@mui/material";
 import { useAppSelector } from "../../redux/store/hooks";
+import { paperStyle } from "../styles";
+
+const loginPaperStyle = {
+    ...paperStyle,
+    width: 400,
+};
 
 const Profile = () => {
     const { user, token } = useAppSelector((state) => state.auth);
     if (!token || !user) return <div>Not loggedIn</div>;
 
     return (
-        <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-                sx={{ height: 140 }}
-                image={user.avatar}
-                title="profile"
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    {user.name}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    {user.email}
-                </Typography>
-            </CardContent>
-        </Card>
+        <Paper elevation={1} style={loginPaperStyle}>
+            <Grid
+                alignItems="center"
+                justifyContent="center"
+                style={{ textAlign: "center" }}
+            >
+                <Card sx={{ maxWidth: 345 }}>
+                    <CardMedia
+                        sx={{ height: 140 }}
+                        image={user.avatar}
+                        title="profile"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            {user.name}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            {user.email}
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </Grid>
+        </Paper>
     );
 };
 
